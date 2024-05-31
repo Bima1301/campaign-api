5e14b2568eec3c27a8772be3aa56dccc454f0ff9
Bima1301 <bima.aji1380@gmail.com>
2024-05-31 13:04:59 +0700

    [FEAT] add create campaign api

--- campaign/input.go
+++ campaign/input.go
@@ -1,5 +1,16 @@
 package campaign
 
+import "campaign-api/user"
+
 type GetCampaignDetailInput struct {
 	ID string `uri:"id" binding:"required"`
+}
+
+type CreateCampaignInput struct {
+	Name             string `json:"name" binding:"required,max=255"`
+	ShortDescription string `json:"short_description" binding:"required,max=1500"`
+	Description      string `json:"description" binding:"required,max=1500"`
+	GoalAmount       int    `json:"goal_amount" binding:"required"`
+	Perks            string `json:"perks" binding:"required"`
+	User             user.User
 }
--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,17 @@
 package campaign
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/gosimple/slug"
+	"github.com/oklog/ulid/v2"
+)
+
 type Service interface {
 	GetCampaigns(userID string) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -35,4 +44,25 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 		return campaign, err
 	}
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.ID = ulid.Make().String()
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.GoalAmount = input.GoalAmount
+	campaign.Perks = input.Perks
+	campaign.UserID = input.User.ID
+
+	//create slig from name,user_id,and timestamp
+	slugString := fmt.Sprintf("%s %s %s", input.Name, input.User.ID, time.Now().Format("2006-01-02-15:04:05"))
+	campaign.Slug = slug.Make(slugString)
+
+	newCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+	return newCampaign, nil
 }
